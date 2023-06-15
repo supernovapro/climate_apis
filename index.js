744cfbe7ac458d1570231388fe532f77c497cbe7
Mohmmed Abdullah <38705916+supernovapro@users.noreply.github.com>
2023-06-15 18:38:03 +0100

    scrape tow news website
    inseted of using too much website to scrape i use just tow website

--- index.js
+++ index.js
@@ -11,23 +11,8 @@ const newspapers = [
         base: ''
     },
     {
-        name: 'thetimes',
-        address: 'https://www.thetimes.co.uk/environment/climate-change',
-        base: ''
-    },
-    {
-        name: 'guardian',
-        address: 'https://www.theguardian.com/environment/climate-crisis',
-        base: '',
-    },
-    {
-        name: 'telegraph',
-        address: 'https://www.telegraph.co.uk/climate-change',
-        base: 'https://www.telegraph.co.uk',
-    },
-    {
         name: 'nyt',
-        address: 'https://www.nytimes.com/international/section/climate',
+        address: 'https://www.nytimes.com/international',
         base: '',
     },
     {
@@ -50,82 +35,62 @@ const newspapers = [
         address: 'https://www.bbc.co.uk/news/science_and_environment',
         base: 'https://www.bbc.co.uk',
     },
-    {
-        name: 'es',
-        address: 'https://www.standard.co.uk/topic/climate-change',
-        base: 'https://www.standard.co.uk'
-    },
-    {
-        name: 'sun',
-        address: 'https://www.thesun.co.uk/topic/climate-change-environment/',
-        base: ''
-    },
-    {
-        name: 'dm',
-        address: 'https://www.dailymail.co.uk/news/climate_change_global_warming/index.html',
-        base: ''
-    },
-    {
-        name: 'nyp',
-        address: 'https://nypost.com/tag/climate-change/',
-        base: ''
-    }
+ 
 ]
+  axios.get('https://www.smh.com.au/world')
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const baseUrl = 'https://www.smh.com.au'
+    // Extract data from the page using Cheerio selectors
 
+    $('div[data-an-position]').each((_, element) => {
+      const article = {};
+      const linkElement = $(element).find('h3 a');
+      article.title = linkElement.text();
+      article.url = baseUrl+ linkElement.attr('href');
+    
+      const imageElement = $(element).find('img');
+      article.imageUrl = imageElement.attr('src');
+      article.source = 'THE SYDNEY MORNING HERALD';
+
+      articles.push(article);
+    });
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 const articles = []
-
-newspapers.forEach(newspaper => {
-    axios.get(newspaper.address)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-
-            $('a:contains("climate")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-
-                articles.push({
-                    title,
-                    url: newspaper.base + url,
-                    source: newspaper.name
-                })
-            })
-
-        }).catch(err => console.log(err))
-})
-
 app.get('/', (req, res) => {
-    res.json('Welcome to my Climate Change News API')
+     res.json('<h1>wellcome to Super Nova Pro trend news, News </h1>')
 })
 
-app.get('/news', (req, res) => {
+app.get('/SNovaTrendsNews', (req, res) => {
     res.json(articles)
 })
 
-app.get('/news/:newspaperId', (req, res) => {
-    const newspaperId = req.params.newspaperId
+axios.get('https://www.aljazeera.com/')
+  .then((response) => {
+    const baseLink = 'https://www.aljazeera.com'
 
-    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
-    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+    const html = response.data;
+    const $ = cheerio.load(html);
+  $('a[href^="/news/"]').each((_, element) => {
+    const article = {};
 
+    article.title =  $(element).text();
+    article.url = baseLink + $(element).attr('href');
 
-    axios.get(newspaperAddress)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const specificArticles = []
+    const imageElement = $('div.fte-article__featured-image img');
+    article.imageUrl =baseLink + imageElement.attr('src');
+    article.source = 'aljazeera';
 
-            $('a:contains("climate")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-                specificArticles.push({
-                    title,
-                    url: newspaperBase + url,
-                    source: newspaperId
-                })
-            })
-            res.json(specificArticles)
-        }).catch(err => console.log(err))
-})
+    articles.push(article);
+   });
+    // Process and use the extracted data as needed
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
